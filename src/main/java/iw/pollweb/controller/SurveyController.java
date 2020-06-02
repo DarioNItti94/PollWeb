def6306b12e56a1023a444bd60e061cb2bd30082
DarioNitt <dariocontest@live.it>
2020-06-02 11:46:13 +0200

    Finish survey controller(done)

--- src/main/java/iw/pollweb/controller/SurveyController.java
+++ src/main/java/iw/pollweb/controller/SurveyController.java
@@ -6,7 +6,9 @@
 package iw.pollweb.controller;
 
 import iw.framework.data.DataException;
+import iw.framework.result.SplitSlashesFmkExt;
 import iw.framework.result.TemplateManagerException;
+import iw.framework.result.TemplateResult;
 import iw.framework.security.SecurityLayer;
 import iw.pollweb.model.PollWebDataLayer;
 import iw.pollweb.model.dto.Supervisor;
@@ -19,6 +21,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -26,30 +29,29 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "SurveyController", urlPatterns = {"/survey"})
 public class SurveyController extends BaseController {
-    
-    private void action_default(HttpServletRequest request, HttpServletResponse response) throws ServerException, IOException, TemplateManagerException{
-        
-    }
 
-    private void action_open(HttpServletRequest request, HttpServletResponse response) throws ServerException, IOException, DataException, ServletException {
-        int surveyID = SecurityLayer.checkNumeric(request.getParameter("id"));
-        Survey survey = ((PollWebDataLayer) request.getAttribute("datalayer")).getSurveyDAO().getSurveyByID(surveyID);
-        if (survey.isClosed()) {
-            throw new ServletException("Il songaggio è chiuso");
-        } else if (survey != null) {
-            survey.setActive(true);
-            response.sendRedirect("survey?survey=" + surveyID);
+    private void action_default(HttpServletRequest request, HttpServletResponse response) throws ServerException, IOException, TemplateManagerException, DataException {
+        int surveyid = SecurityLayer.checkNumeric(request.getParameter("survey"));
+        Survey survey = ((PollWebDataLayer) request.getAttribute("datalayer")).getSurveyDAO().getSurveyByID(surveyid);
+        request.setAttribute("survey", survey);
+        HttpSession s = SecurityLayer.checkSession(request);
+        TemplateResult res = new TemplateResult(getServletContext());
+        request.setAttribute("split_shalshes", new SplitSlashesFmkExt());
+        request.setAttribute("page_title", "login");
+        res.activate("/survey.ftl.html", request, response);
+        if (survey.isClosed() && survey.isActive()) {
+            res.activate("/error.ftl.html", request, response);
+            request.setAttribute("error", "il sondaggio è chiuso");
         } else {
-
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            response.sendRedirect("survey?survey=" + surveyid);
         }
+
     }
 
     private void action_delete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, DataException {
         int surveyID = SecurityLayer.checkNumeric(request.getParameter("id"));
         Survey survey = ((PollWebDataLayer) request.getAttribute("datalayer")).getSurveyDAO().getSurveyByID(surveyID);
         if (survey != null) {
-
             ((PollWebDataLayer) request.getAttribute("datalayer")).getSurveyDAO().deleteSurvey(surveyID);
             response.sendRedirect("profile");
         } else {
@@ -93,20 +95,32 @@ public class SurveyController extends BaseController {
         }
     }
 
-    private void action_dactivate(HttpServletRequest request, HttpServletResponse response) throws ServerException, DataException {
+    private void action_deactivate(HttpServletRequest request, HttpServletResponse response) throws ServerException, DataException, IOException {
         int surveyID = SecurityLayer.checkNumeric(request.getParameter("id"));
         Survey survey = ((PollWebDataLayer) request.getAttribute("datalayer")).getSurveyDAO().getSurveyByID(surveyID);
-        if(survey != null){
+        if (survey != null) {
             survey.setActive(false);
-            response.sendRedirect("");
+            response.sendRedirect("profile");
         }
-       
-        
+
     }
 
     @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, DataException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            if(request.getParameter("action_dafault")!= null){
+                action_default(request, response);
+            }else if(request.getParameter("action_create")!= null){
+                action_create(request, response);
+            }else if(request.getParameter("action_delete") != null){
+                action_delete(request, response);
+            }else if(request.getParameter("action_close") != null){
+                action_close(request, response);
+            }else if (request.getParameter("action_deactivate")!= null) {
+                action_deactivate(request, response);
+            }
+        } catch (Exception e) {
+        }
     }
 
     @Override
